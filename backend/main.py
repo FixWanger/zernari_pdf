49773a4831419150cf972f0062f58b4731918fe7
FixWanger <egorzheznak07@gmail.com>
2026-05-31 19:20:19 +0300

    Fix CORS policy to allow Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -17,9 +17,15 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Zernari PDF API (Secured)")
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://zernari-pdf.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
